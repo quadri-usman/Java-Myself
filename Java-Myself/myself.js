baa533a25630ffe83cc0cdf720dc053ba8ec7eb2
quadri-usman <105228467+quadri-usman@users.noreply.github.com>
2022-06-20 10:48:57 -0700

    Add files via upload

---
+++ Java-Myself/myself.js
@@ -0,0 +1,4 @@
+var h1 = document.querySelector("h1");
+h1.textContent = "Quadri Taiwo USMAN";
+document.querySelectorAll("p")[0].textContent = "My height is 1.2m";
+document.querySelectorAll("p")[1].textContent = "My country is Nigeri";
