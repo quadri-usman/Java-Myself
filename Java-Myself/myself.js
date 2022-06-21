6cbde30657a7facb5e828ec940f3c2f7352fdd38
quadri-usman <105228467+quadri-usman@users.noreply.github.com>
2022-06-20 23:25:55 -0700

    Update myself.js

--- Java-Myself/myself.js
+++ Java-Myself/myself.js
@@ -1,4 +1,12 @@
 var h1 = document.querySelector("h1");
 h1.textContent = "Quadri Taiwo USMAN";
 document.querySelectorAll("p")[0].textContent = "My height is 1.2m";
-document.querySelectorAll("p")[1].textContent = "My country is Nigeri";
+document.querySelectorAll("p")[1].textContent = "My country is Nigeria";
+
+
+var myself = {
+    name: "Quadri Taiwo USMAN",
+    height: "1.2m",
+    country: "Nigeria"
+  }
+  console.log(myself)
